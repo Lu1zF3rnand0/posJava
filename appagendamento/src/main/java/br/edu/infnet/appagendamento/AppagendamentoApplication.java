ede57c2f0465c48af89152c02e8dc0bf81a279f7
Lu1zF3rnand0 <luizfernando.silva@outlook.com>
2021-11-08 10:45:42 -0300

    Commit Teste

--- appagendamento/src/main/java/br/edu/infnet/appagendamento/AppagendamentoApplication.java
+++ appagendamento/src/main/java/br/edu/infnet/appagendamento/AppagendamentoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AppagendamentoApplication {
-    //teste
+    //test
 	public static void main(String[] args) {
 		SpringApplication.run(AppagendamentoApplication.class, args);
 	}
