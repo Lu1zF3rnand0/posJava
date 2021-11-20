1737b6a73f5fb3fb2910b51c882efa00fced5d82
Lu1zF3rnand0 <luizfernando.silva@outlook.com>
2021-11-20 13:52:22 -0300

    Atualizando projeto.

--- appagendamentocarga/src/main/java/br/edu/infnet/appagendamentocarga/AppagendamentocargaApplication.java
+++ appagendamentocarga/src/main/java/br/edu/infnet/appagendamentocarga/AppagendamentocargaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AppagendamentocargaApplication {
-//
+
 	public static void main(String[] args) {
 		SpringApplication.run(AppagendamentocargaApplication.class, args);
 	}
